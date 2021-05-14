2d7cab56fdf8bfd1e1500098cc8aee2526cacde5
李彦凯 <yancy@hightopo.com>
2021-05-14 12:07:34 +0800

    完善 RefViewGroup 相关属性配置

--- src/prop-config/refviewgroup.js
+++ src/prop-config/refviewgroup.js
@@ -14,11 +14,34 @@ export default function(uiEditor) {
             {
                 name: 'RefViewGroup',
                 displayName: uiEditor.getString('toolkit.refviewgroup'),
-                properties: [{
+                properties: [
+                    {
                         name: 'url',
                         displayName: uiEditor.getString('editor.property.url'),
-                        type: 'string'
-                    }
+                        type: 'slotResource'
+                    },
+                    {
+                        name: 'slotreplaceable',
+                        displayName: uiEditor.getString('editor.property.slotreplaceable'),
+                        type: 'boolean',
+                        getValue: function (view, property) {
+                            return view.a('slotReplaceable');
+                        },
+                        setValue: function (view, value, property) {
+                            view.a('slotReplaceable', !!value)
+                        }
+                    },
+                    // {
+                    //     name: 'slotName',
+                    //     displayName: uiEditor.getString('editor.property.slotname'),
+                    //     type: 'string',
+                    //     getValue: function (view, property) {
+                    //         return view.a('slot');
+                    //     },
+                    //     setValue: function (view, value, property) {
+                    //         view.a('slot', value)
+                    //     }
+                    // }
                 ]
             }
         ]
