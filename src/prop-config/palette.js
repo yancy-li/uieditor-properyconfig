c6016742a0609e68a343aaf6445ef6335dee2bed
李彦凯 <yancy@hightopo.com>
2021-09-13 16:25:13 +0800

    palette 支持配置面板和内容按钮样式；

--- src/prop-config/palette.js
+++ src/prop-config/palette.js
@@ -73,6 +73,118 @@ export default function(uiEditor) {
                         type: 'paletteItems'
                     }
                 ]
+            },
+            {
+                displayName: uiEditor.getString('toolkit.palettepanel'),
+                getValue: function (view, property) {
+                    var templatePanel = view.getTemplatePanel();
+                    return templatePanel[ht.Default.prefixGetter(property.name)]();
+                },
+                setValue: function (view, value, property) {
+                    var templatePanel = view.getTemplatePanel();
+                    templatePanel[ht.Default.prefixSetter(property.name)](value);
+                },
+                extends: [
+                    {
+                        rule: 'ht.ui.Panel',
+                        categoryId: 'basic',
+                        filter: [
+                            'background'
+                        ]
+                    },
+                    {
+                        rule: 'ht.ui.Panel',
+                        categoryId: 'panel',
+                        filter: [
+                            'headerHeight', 'is:headerVisible', 'headerBackground', 'hoverHeaderBackground', 'activeHeaderBackground',
+                            'titleFont', 'titleColor', 'hoverTitleColor', 'activeTitleColor', 'iconWidth', 'iconHeight', 'tools', 'toolWidth', 'toolHeight',
+                            'contentPadding', 'expandIcon', 'hoverExpandIcon', 'activeExpandIcon', 'collapseIcon', 'hoverCollapseIcon', 'activeCollapseIcon'
+                        ]
+                    }
+                ]
+            },
+            {
+                displayName: uiEditor.getString('toolkit.palettebutton'),
+                getValue: function (view, property) {
+                    var templateButton = view.getTemplateButton();
+                    return templateButton[ht.Default.prefixGetter(property.name)]();
+                },
+                setValue: function (view, value, property) {
+                    var templateButton = view.getTemplateButton();
+                    templateButton[ht.Default.prefixSetter(property.name)](value);
+                },
+                extends: [
+                    {
+                        rule: 'ht.ui.View',
+                        categoryId: 'basic',
+                        filter: [
+                            'background', 'border', 'padding', 'borderRadius', 
+                            {
+                                name: 'preferredSize',
+                                displayName: uiEditor.getString('editor.property.preferredsize'),
+                                type: 'string',
+                                setValue: function(view, value, property) {
+                                    view = view.getTemplateButton();
+                                    if (value == null || value.trim() == '') {
+                                        view.setPreferredSize(undefined);
+                                    }
+                                    else {
+                                        value = value.split(',');
+                                        var width = value[0];
+                                        var height = value[1];
+                                        if (!isNaN(width)) {
+                                            width = parseInt(width);
+                                        }
+                                        else {
+                                            width = undefined;
+                                        }
+            
+                                        if (!isNaN(height)) {
+                                            height = parseInt(height);
+                                        }
+                                        else {
+                                            height = undefined;
+                                        }
+            
+                                        view.setPreferredSize(width, height);
+                                    }
+                                },
+                                getValue: function(view, property) {
+                                    view = view.getTemplateButton();
+                                    var preferredSize = view._preferredSize;
+                                    if (preferredSize && view.isPreferredSizeSet()) {
+                                        var str = '';
+                                        var width = preferredSize.width,
+                                            height = preferredSize.height;
+                                        if (width != null) {
+                                            str += width;
+                                        }
+                                        str += ',';
+            
+                                        if (height != null) {
+                                            str += height;
+                                        }
+                                        return str;
+                                    }
+                                }
+                            },
+                            'boxShadow'
+                        ]
+                    },
+                    {
+                        rule: 'ht.ui.ToggleButton',
+                        categoryId: 'togglebutton',
+                        filter: [
+                            'textFont', 'textColor', 'hoverTextColor', 'activeTextColor', 
+                            'selectTextColor', 'selectHoverTextColor', 'selectActiveTextColor', 
+                            'hTextPosition', 'vTextPosition', 
+                            'hoverBackground', 'activeBackground','selectBackground','selectHoverBackground',
+                            'selectActiveBackground','iconWidth', 'iconHeight', 'iconTextGap', 'iconStretch', 
+                            'toolTipEnabled', 'is:pixelPerfect',
+                            'align', 'vAlign'
+                        ]
+                    }
+                ]
             }
         ]
     }
